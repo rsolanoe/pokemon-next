b9406b5cf994697203d16ca4be364396b68a1ab8
Rodolfo Solano <rjse31@gmail.com>
2022-08-08 20:37:06 -0500

    prueba

---
+++ pages/prueba/index.tsx
@@ -0,0 +1,13 @@
+import { NextPage } from 'next';
+
+
+const index: NextPage = () => {
+  return (
+    <div>
+        <div>index</div>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat accusantium mollitia exercitationem provident officia repellendus voluptatum. Inventore blanditiis unde vero? Totam architecto nesciunt eos! Aperiam quibusdam ex, eaque similique exercitationem praesentium minima tempora maiores laborum voluptas voluptatibus perferendis nulla eum omnis fugit nesciunt cum, ad magni, dolorem ea numquam fugiat!</p>
+    </div>
+  )
+}
+
+export default index
